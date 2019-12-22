bb189ebbb128e1781016a4cc33b75210864b8d89
Igor Akkerman <kata@igorakkerman.de>
2019-12-22 01:22:55 +0100

    array iteration optimized

--- maxsameevenodd.go
+++ maxsameevenodd.go
@@ -9,8 +9,8 @@ func MaxSame(a []int) int {
 	odd := a[1]
 	last := 2
 	max := 2
-	for i := 2; i < len(a); i++ {
-		if i%2 == 0 && a[i] == even || i%2 == 1 && a[i] == odd {
+	for i, value := range a[2:] {
+		if i%2 == 0 && value == even || i%2 == 1 && value == odd {
 			last++
 			if last > max {
 				max = last
@@ -19,9 +19,9 @@ func MaxSame(a []int) int {
 			last = 2
 		}
 		if i%2 == 0 {
-			even = a[i]
+			even = value
 		} else {
-			odd = a[i]
+			odd = value
 		}
 	}
 
